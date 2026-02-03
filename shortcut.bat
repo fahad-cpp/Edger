1c83baf8c524402903e5309eef856a5b4e3f002a
Ibrahim Shaikh <unknownalpha030705@gmail.com>
2026-02-03 17:11:45 +0530

    Fixed icon not showing

--- shortcut.bat
+++ shortcut.bat
@@ -3,7 +3,7 @@ setlocal
 set BASEDIR=%~dp0
 set SHORTCUT=%BASEDIR%\Edger.lnk
 set STARTIN=%BASEDIR%
-set ICON=%BASEDIR%\icon.ico
+set ICON=%BASEDIR%icon.ico
 set EXE=%BASEDIR%\bin\Edger.exe
 
 if exist %SHORTCUT% del %SHORTCUT%
